0e3356c940ed390a3324663519282d1600093eb7
JanosBanocziRuof <67029836+JanosBanocziRuof@users.noreply.github.com>
2024-04-11 11:38:06 -0400

    Create mongo-commands.js

---
+++ src/mongo-commands.js
@@ -0,0 +1,60 @@
+const { MongoClient } = require('mongodb');
+
+const url = 'mongodb://localhost:27017';
+const mongo = new MongoClient(url);
+
+const db_name = 'ninjaio';
+const collection_lb = 'leaderboard';
+
+const leaderboardCollection = mongo.db(db_name).collection(collection_lb);
+
+const connectToDatabase = async () => {
+    try {
+        await mongo.connect();
+        console.log(`Connected to the ${dbname} database.`);
+    } catch (err) {
+        console.log(`Error connecting to the database: ${err}`);
+    }
+};
+
+const main = async () => {
+    try {
+        await connectToDatabase();
+        let result = await leaderboardCollection.insertOne();
+        console.log('Yay');
+    } catch (err) {
+        console.error(`Error: ${err}`)
+    } finally {
+        await client.close();
+    }
+}
+
+// db structure for reference
+const mockup = [
+    {
+        weaponName: 'rpg',
+        weaponId: 8,
+        currentTotalKills: 23115615,
+        lastUpdated: new Date(),
+        rankingsArchive: [
+            {
+                date: new Date(),
+                totalKills: 23115615,
+                rankings: [
+                    { name: 'DarkMaster', accountId: 40008, kills: 54176 },
+                    { name: 'Basil', accountId: 75701, kills: 35484 },
+                    // etc
+                ]
+            },
+            {
+                date: new Date(),
+                totalKills: 23115615,
+                rankings: [
+                    { name: 'DarkMaster', accountId: 40008, kills: 54176 },
+                    { name: 'Basil', accountId: 75701, kills: 35484 },
+                    // etc
+                ]
+            }
+        ]
+    }
+]
